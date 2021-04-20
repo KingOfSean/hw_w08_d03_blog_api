512b98021d7f63120256b2e4e3cf144d7916ac01
kingofsean <seanking2380@gmail.com>
2021-04-20 18:38:56 -0500

    finished api forreal

--- blog_root/blog/settings.py
+++ blog_root/blog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,12 +41,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'homework.apps.HomeworkConfig',
     'rest_framework',
+    'corsheaders'
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+ROOT_URLCONF = 'homeworkproject.urls'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -125,3 +133,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
